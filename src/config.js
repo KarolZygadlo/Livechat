31177d32f3a8490439d30127f08a944e554fa443
Karol Zygadło <k.zygadlo@adawards.pl>
2020-02-23 16:27:51 +0100

    first version of app, added firebase support, simple chat system, google authorization

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDosJldyApBaOujAMomDTwXssfeCKFE0EM",
+    authDomain: "chatappreact-c3c4e.firebaseapp.com",
+    databaseURL: "https://chatappreact-c3c4e.firebaseio.com",
+    projectId: "chatappreact-c3c4e",
+    storageBucket: "chatappreact-c3c4e.appspot.com",
+    messagingSenderId: "936326076471",
+    appId: "1:936326076471:web:2bb8cf36ad07ab2b8bc7b2",
+    measurementId: "G-SSZVGX5Q50"
+  };
