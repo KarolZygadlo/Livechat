f566f0cee5f83893a75fef2f3022711a7714c702
Karol Zygadło <k.zygadlo@adawards.pl>
2020-04-25 16:31:48 +0200

    begin the new start of application, installation and import of basic packs, creating routes

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,8 @@
-import firebase from 'firebase'
+import React from 'react';
+import * as firebase from 'firebase';
 
-const config = {
+
+var  firebaseConfig = {
   apiKey: "AIzaSyDosJldyApBaOujAMomDTwXssfeCKFE0EM",
   authDomain: "chatappreact-c3c4e.firebaseapp.com",
   databaseURL: "https://chatappreact-c3c4e.firebaseio.com",
@@ -10,11 +12,10 @@ const config = {
   appId: "1:936326076471:web:2bb8cf36ad07ab2b8bc7b2",
   measurementId: "G-SSZVGX5Q50"
 }
-firebase.initializeApp(config)
+
+firebase.initializeApp(firebaseConfig)
 firebase.firestore().settings({
     timestampsInSnapshots: true
 })
 
-export const myFirebase = firebase
-export const myFirestore = firebase.firestore()
-export const myStorage = firebase.storage()
+export default firebase;
